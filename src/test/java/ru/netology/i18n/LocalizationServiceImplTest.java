c30c34f7e470f9d11eea93afc5979aba0edb64ce
Maksim Zharinov <minimum.cold1991@gmail.com>
2024-05-19 01:13:44 +0700

    create test classes

---
+++ src/test/java/ru/netology/i18n/LocalizationServiceImplTest.java
@@ -0,0 +1,12 @@
+package ru.netology.i18n;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LocalizationServiceImplTest {
+
+    @Test
+    void locale() {
+    }
+}
