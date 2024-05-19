d9d78ac87601fc254144be0b1127c7a3d1ea7f7b
Maksim Zharinov <minimum.cold1991@gmail.com>
2024-05-20 03:44:45 +0700

    create test for GeoServiceImpl

--- src/test/java/ru/netology/geo/GeoServiceImplTest.java
+++ src/test/java/ru/netology/geo/GeoServiceImplTest.java
@@ -1,12 +1,25 @@
 package ru.netology.geo;
 
-import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import ru.netology.entity.Country;
+import ru.netology.entity.Location;
 
 class GeoServiceImplTest {
 
-    @Test
-    void byIp() {
+    @ParameterizedTest
+    @CsvSource(value = {
+            "172., RUSSIA",
+            "96., USA"
+    })
+    void byIpTest(String ip, Country country) {
+        GeoService geoService = new GeoServiceImpl();
+
+        Location result = geoService.byIp(ip);
+        Country expected = country;
+
+        Assertions.assertEquals(expected, result.getCountry());
     }
 }
