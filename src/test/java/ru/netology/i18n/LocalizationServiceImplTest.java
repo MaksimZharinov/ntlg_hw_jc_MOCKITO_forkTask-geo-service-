461324742b2946b130edb2a389685732f84e5a16
Maksim Zharinov <minimum.cold1991@gmail.com>
2024-05-20 03:48:35 +0700

    create test for LocalizationServiceImpl

--- src/test/java/ru/netology/i18n/LocalizationServiceImplTest.java
+++ src/test/java/ru/netology/i18n/LocalizationServiceImplTest.java
@@ -1,12 +1,26 @@
 package ru.netology.i18n;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import ru.netology.entity.Country;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class LocalizationServiceImplTest {
 
-    @Test
-    void locale() {
+    @ParameterizedTest
+    @CsvSource(value = {
+            "Welcome, USA",
+            "Добро пожаловать, RUSSIA"
+    })
+    void localeTest(String msg, Country country) {
+        LocalizationService localizationService = new LocalizationServiceImpl();
+
+        String result = localizationService.locale(country);
+        String expected = msg;
+
+        Assertions.assertEquals(expected, result);
     }
 }
