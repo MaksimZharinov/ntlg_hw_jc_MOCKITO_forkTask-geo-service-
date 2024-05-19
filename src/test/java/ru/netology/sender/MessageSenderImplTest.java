548d429cb3d4d037e8869af3450ad41b9a16a5c3
Maksim Zharinov <minimum.cold1991@gmail.com>
2024-05-20 03:44:03 +0700

    change tests in MessageSenderImplTest

--- src/test/java/ru/netology/sender/MessageSenderImplTest.java
+++ src/test/java/ru/netology/sender/MessageSenderImplTest.java
@@ -3,6 +3,7 @@ package ru.netology.sender;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import ru.netology.entity.Country;
 import ru.netology.entity.Location;
 import ru.netology.geo.GeoService;
 import ru.netology.geo.GeoServiceImpl;
@@ -20,9 +21,9 @@ class MessageSenderImplTest {
     @Test
     void sendRu() {
         GeoService geoService = Mockito.mock(GeoServiceImpl.class);
-        Mockito.when(geoService.byIp("172.")).thenReturn(new Location("Moscow", RUSSIA, null, 0));
+        Mockito.when(geoService.byIp("172.")).thenReturn(new Location("Moscow", Country.RUSSIA, null, 0));
         LocalizationService localizationService = Mockito.mock(LocalizationServiceImpl.class);
-        Mockito.when(localizationService.locale(RUSSIA)).thenReturn("Добро пожаловать");
+        Mockito.when(localizationService.locale(Country.RUSSIA)).thenReturn("Добро пожаловать");
         MessageSender messageSender = new MessageSenderImpl(geoService, localizationService);
         Map<String, String> headers = new HashMap<>();
         headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, "172.");
